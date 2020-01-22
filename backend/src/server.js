a6b1072d4e96a96ab410578827a38c869ccdca7e
fucitalo <38429356+fucitalo@users.noreply.github.com>
2020-01-22 19:08:16 -0300

    Upload dos arquivos

---
+++ backend/src/server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const routes = require('./routes');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
+const socketio = require('socket.io');
+const http = require('http');
+
+const app = express();
+const server = http.Server(app);
+const io = socketio(server); 
+
+mongoose.connect('mongodb+srv://jonas:reis@os-vl9k5.mongodb.net/OS?retryWrites=true&w=majority',{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+});
+
+const connectedUsers = {};
+
+io.on('connection',socket=>{
+    const  {user_id} = socket.handshake.query;
+    connectedUsers[user_id] = socket.id;
+})
+
+app.use((req, res, next)=>{
+    req.io = io;
+    req.connectedUsers = connectedUsers;
+
+    return next();
+})
+
+// req.query - Acessar query params (filtros)
+// req.params - Acessar route params (edit, delet)
+// req.body - Acessar corpo da requisição
+
+/*app.get('/users', (req, res)=>{
+    return res.json({"idade":req.query.idade});
+});*/
+
+app.use(cors());
+app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
+app.use(routes);
+
+server.listen(3334);
